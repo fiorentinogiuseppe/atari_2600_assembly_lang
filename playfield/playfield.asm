f41de81c12f757cda6a2a690623d389b5d479c36
fiorentinogiuseppe <fiorentinogiuseppebcc@gmail.com>
2020-05-25 17:25:50 -0300

    playfield finalized

--- playfield/playfield.asm
+++ playfield/playfield.asm
@@ -1,6 +1,7 @@
 	processor 6502
 	include "vcs.h"
 	include "macro.h"
+        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Start our ROM code
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -9,10 +10,10 @@
 Reset:
 	CLEAN_START
         
-        ldx #$80
+        ldx #$80		; blue background color	
         stx COLUBK
         
-        lda #$1C
+        lda #$1C		; yellow playfield color
         sta COLUPF
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -20,21 +21,103 @@ Reset:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 StartFrame:
 	lda #02
-        sta VBLANK
-        sta VSYNC
+        sta VBLANK		; turn VBLANK on
+        sta VSYNC		; turn VSYNC on
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Generate the 3 lines of VSYNC
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+	REPEAT 3
+        	sta WSYNC	; 3 scanlines for VSYNC
+        REPEND
+        lda #0
+        sta VSYNC		; turn off VSYNC
+        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Let the TIA output the 37 recommended lines of VBLANK
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+	REPEAT 37
+        	sta WSYNC
+        REPEND
+        lda #0
+        sta VBLANK		; turn off VBLANK
+        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set the CTRLPF register to allow playerfield reflection
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+	ldx #$0000001		; CTRLPF register (D0 means reflect the PF)
+        stx CTRLPF
+        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Draw the 192 visible scanlines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; Skip 7 scanlines with no PF set
+        ldx #0
+        stx PF0
+        stx PF1
+        stx PF2
+        REPEAT 7
+        	sta WSYNC
+        REPEND
+        
+        ; Set the PF0 to 1110 (LSB first) and PF1-PF2 as 1111 1111
+        ldx #%11100000
+        stx PF0
+        ldx #%11111111
+        stx PF1
+        stx PF2
+        REPEAT 7
+        	sta WSYNC
+        REPEND
+        
+        ; Set the next 164 lines only with PF0 third bit enabled
+        ldx #%00100000
+        stx PF0
+        ldx #0
+        stx PF1
+        stx PF2
+        REPEAT 164
+        	sta WSYNC
+        REPEND
+        
+        ; Set the PF0 to 1110 (LSB first) and PF1-PF2 as 1111 1111
+        ldx #%11100000
+        stx PF0
+        ldx #%11111111
+        stx PF1
+        stx PF2
+        REPEAT 7
+        	sta WSYNC
+        REPEND
+        
+        ; Skip 7 vertical lines with no PF set
+        ldx #0
+        stx PF0
+        stx PF1
+        stx PF2
+        REPEAT 7
+        	sta WSYNC
+        REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Outplut 30 more VBLANK overscan lines to complete our frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	lda #2
+        sta VBLANK
+        REPEAT 30
+        	sta WSYNC
+        REPEND
+        lda #0
+        sta VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Loop to next frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	jmp StartFrame
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Complete ROM Size
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	org $fffc
+        .word Reset
+        .word Reset
